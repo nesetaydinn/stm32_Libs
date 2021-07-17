5cacbc2c13640cc8af521ba2231bdee4294fc5f3
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2021-07-17 03:39:15 +0300

    Update na_Ws2812DRV.h

--- na_Ws2812DRV/na_Ws2812DRV.h
+++ na_Ws2812DRV/na_Ws2812DRV.h
@@ -12,10 +12,15 @@
 #include "stdint.h"
 #include "stdbool.h"
 
-#define MAX_LED_SIZE 32
+#define NA_WS2812_MAX_LED_SIZE 32
+#define NA_WS2812_LED_BRIGHT_COMPLEX 1
 
 typedef struct{
-	uint32_t ledsArr[MAX_LED_SIZE][3];
+	#if NA_WS2812_LED_BRIGHT_COMPLEX
+	uint32_t ledsArr[NA_WS2812_MAX_LED_SIZE][4];
+	#else
+	uint32_t ledsArr[NA_WS2812_MAX_LED_SIZE][3];
+	#endif
 	uint8_t led_arrSize;
 	uint8_t bright;
 	bool dataSendFlag;
@@ -29,11 +34,20 @@ typedef struct{
 na_Ws2812  na_Ws2812_create(TIM_HandleTypeDef *htim,
 		uint32_t Channel,
 		uint8_t led_arrSize);
-void na_Ws2812_putColorIndex(na_Ws2812  *drv,uint8_t index,
+
+#if NA_WS2812_LED_BRIGHT_COMPLEX
+void  na_Ws2812_putColorIndex(na_Ws2812* drv,uint8_t index,
+		uint8_t r,uint8_t g,uint8_t b, uint8_t bright);
+#else
+void  na_Ws2812_putColorIndex(na_Ws2812* drv,uint8_t index,
 		uint8_t r,uint8_t g,uint8_t b);
+
+void na_Ws2812_setBright(na_Ws2812  *drv,uint8_t bright);
+#endif
 void na_Ws2812_send(na_Ws2812 *drv);
 void na_Ws2812_setFlag(na_Ws2812  *drv,bool flag);
-void na_Ws2812_setBright(na_Ws2812  *drv,uint8_t bright);
 bool na_Ws2812_getFlag(na_Ws2812  *drv);
+
+
 
 #endif /* NA_WS2812DRV_H_ */
