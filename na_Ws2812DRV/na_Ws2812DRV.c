12f44c29e50a8ce06df5a83ae63902d94d04eca6
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2021-07-17 03:26:15 +0300

    Update na_Ws2812DRV.c

--- na_Ws2812DRV/na_Ws2812DRV.c
+++ na_Ws2812DRV/na_Ws2812DRV.c
@@ -5,18 +5,20 @@
  *      Author: nesetaydin
  */
 #include "na_Ws2812DRV.h"
+
+
 uint16_t na_Ws2812_valuesMap(uint16_t inValue, uint16_t inMin, uint16_t inMax, uint16_t outMin, uint16_t outMax);
 
 na_Ws2812 na_Ws2812_create(TIM_HandleTypeDef *htim,
-		DMA_HandleTypeDef *hdma_tim,
 		uint32_t Channel,
 		uint8_t led_arrSize){
 	na_Ws2812 tmp;
 	tmp.htim =htim;
-	tmp.hdma_tim=hdma_tim;
 	tmp.htimChannel =Channel;
 	tmp.bright=100;
 	tmp.led_arrSize=led_arrSize;
+	tmp.T1H=(uint16_t)((htim->Instance->ARR+1)*2/3);
+	tmp.T0H=(uint16_t)((htim->Instance->ARR+1)*1/3);
 	for(uint8_t c1=0;c1<MAX_LED_SIZE;c1++)
 		for(uint8_t c2=0;c2<3;c2++)
 			tmp.ledsArr[c1][c2]=0;
@@ -25,27 +27,27 @@ na_Ws2812 na_Ws2812_create(TIM_HandleTypeDef *htim,
 }
 
 
-na_Ws2812  na_Ws2812_putColorIndex(na_Ws2812 drv,uint8_t index,
+void  na_Ws2812_putColorIndex(na_Ws2812* drv,uint8_t index,
 		uint8_t r,uint8_t g,uint8_t b){
 	//if(index<drv.led_arrSize)return drv;
-	drv.ledsArr[index][0]=g;
-	drv.ledsArr[index][1]=r;
-	drv.ledsArr[index][2]=b;
-	return drv;
+	drv->ledsArr[index][0]=g;
+	drv->ledsArr[index][1]=r;
+	drv->ledsArr[index][2]=b;
+
 }
 
-na_Ws2812 na_Ws2812_send(na_Ws2812  drv){
+void na_Ws2812_send(na_Ws2812  *drv){
 	uint32_t pwmdata_X[(24*MAX_LED_SIZE)+50];
 	uint32_t index=0,color;
-		for(uint8_t c=0;c<drv.led_arrSize;c++){
+		for(uint8_t c=0;c<drv->led_arrSize;c++){
 
-			color=valuesMap(drv.bright,0,100,0,drv.ledsArr[c][0])<<16
-					|valuesMap(drv.bright,0,100,0,drv.ledsArr[c][1])<<8
-					|valuesMap(drv.bright,0,100,0,drv.ledsArr[c][2]);
+			color=na_Ws2812_valuesMap(drv->bright,0,100,0,drv->ledsArr[c][0])<<16
+					|na_Ws2812_valuesMap(drv->bright,0,100,0,drv->ledsArr[c][1])<<8
+					|na_Ws2812_valuesMap(drv->bright,0,100,0,drv->ledsArr[c][2]);
 
 			for(int8_t c=23;c>=0;c--){
-				if(color&(1<<c))pwmdata_X[index]=70;
-				else pwmdata_X[index]=35;
+				if(color&(1<<c))pwmdata_X[index]=drv->T1H;
+				else pwmdata_X[index]=drv->T0H;
 				index++;
 			}
 		}
@@ -53,21 +55,18 @@ na_Ws2812 na_Ws2812_send(na_Ws2812  drv){
 			pwmdata_X[index]=0;
 			index++;
 		}
-		HAL_TIM_PWM_Start_DMA(drv.htim,drv.htimChannel, (uint32_t *)pwmdata_X, index);
-		return drv;
+		HAL_TIM_PWM_Start_DMA(drv->htim,drv->htimChannel, (uint32_t *)pwmdata_X, index);
+
 }
-na_Ws2812 na_Ws2812_setFlag(na_Ws2812  drv,bool flag){
-	drv.dataSendFlag=flag;
-	return drv;
+void na_Ws2812_setFlag(na_Ws2812 * drv,bool flag){
+	drv->dataSendFlag=flag;
 }
-bool na_Ws2812_getFlag(na_Ws2812  drv){
-	return drv.dataSendFlag;
+bool na_Ws2812_getFlag(na_Ws2812  *drv){
+	return drv->dataSendFlag;
 }
-na_Ws2812 na_Ws2812_setBright(na_Ws2812  drv,uint8_t bright){
-	drv.bright=bright;
-	return drv;
+void na_Ws2812_setBright(na_Ws2812 * drv,uint8_t bright){
+	drv->bright=bright;
 }
 uint16_t na_Ws2812_valuesMap(uint16_t inValue, uint16_t inMin, uint16_t inMax, uint16_t outMin, uint16_t outMax) {
 	return (inValue - inMin)*(outMax - outMin) / (inMax - inMin) + outMin;
 }
-
